eec61caf5bae5bab67512f645dc4b033026db4f2
Vladimir Stabrovskiy <123739362+etherealSent@users.noreply.github.com>
2024-09-10 16:01:49 +0300

    http docker-compose

---
+++ lab1/backend/main.py
@@ -0,0 +1,52 @@
+import random
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/items")
+def get_items():
+    items = [
+        {
+            "id": 1,
+            "name": "BigBoy",
+            "img": "https://cdn2.thecatapi.com/images/1su.jpg",
+        },
+        {
+            "id": 2,
+            "name": "Swagger",
+            "img": "https://cdn2.thecatapi.com/images/3lo.jpg",
+        },
+        {
+            "id": 3,
+            "name": "Chiller",
+            "img": "https://cdn2.thecatapi.com/images/an5.jpg",
+        },
+                {
+            "id": 4,
+            "name": "Viber",
+            "img": "https://cdn2.thecatapi.com/images/coa.jpg",
+        },
+    ]
+    random.shuffle(items)
+    return items
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        # "http://31.129.43.117",
+        # "https://site-test-deploy1.ru",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ lab1/frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [items, setItems] = useState(null)
+
+  const fetchItems = () => {
+    axios.get('http://127.0.0.1:8000/items').then(r => {
+      setItems(r.data)
+    })
+  }
+
+  useEffect(() => {
+    fetchItems()
+    setInterval(() => {
+      fetchItems()
+    }, 5000)
+  }, [])
+
+  return (
+    <>
+      {items && items.map(item => {
+        return <span style={{padding:'0px 4px', display: 'flex', flexDirection: 'column', alignItems: 'center'}} key={item.name} className="roll-out">
+        <img src={item.img} alt='logo' width="200" style={{padding:'0px 5px'}}></img>
+        <span>{item.name}</span>
+      </span>
+      })}
+    </>
+  )
+}
+
+export default App
