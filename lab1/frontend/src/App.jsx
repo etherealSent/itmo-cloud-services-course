4f7da07732b4060ca92d4c13a5fa2d44cb998c02
godnesty <ethereal.sent@gmail.com>
2024-09-20 17:34:09 +0300

    ab

--- lab1/frontend/src/App.jsx
+++ lab1/frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <>
       {items && items.map(item => {
         return <span style={{padding:'0px 4px'}} key={item.name} className="roll-out">
-          <img src={item.img} alt='logo' width="60" style={{padding:'0px 5px'}}></img>
+          <img src={item.img} alt='logo' width="90" style={{padding:'0px 5px'}}></img>
           <span>{item.name}</span>
         </span>
       })}
