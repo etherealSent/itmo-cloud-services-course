8ecf9ef5f04b4c350cae5d26b2dc0554d33417e5
godnesty <ethereal.sent@gmail.com>
2024-10-04 15:32:55 +0300

    initial commit

---
+++ lab2/backend/main.py
@@ -0,0 +1,56 @@
+import random
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/items")
+def get_items():
+    items = [
+        {
+            "id": 1,
+            "name": "BigBoy",
+            "img": "https://cdn2.thecatapi.com/images/1su.jpg",
+        },
+        {
+            "id": 2,
+            "name": "Swagger",
+            "img": "https://cdn2.thecatapi.com/images/3lo.jpg",
+        },
+        {
+            "id": 3,
+            "name": "Chiller",
+            "img": "https://cdn2.thecatapi.com/images/an5.jpg",
+        },
+                {
+            "id": 4,
+            "name": "Viber",
+            "img": "https://cdn2.thecatapi.com/images/coa.jpg",
+        },
+    ]
+    random.shuffle(items)
+    return items
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        # "http://localhost:5173",
+        # "http://87.228.16.92"
+        "https://valdemir.ru",
+        "https://www.valdemir.ru",
+        "https://valdemir.online",
+        "https://www.valdemir.online",
+
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ lab2/frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  const fetchItems = () => {
+    axios.get("https://valdemir.online/api/items").then(r => {
+      setItems(r.data)
+    })
+  }
+
+  useEffect(() => {
+    fetchItems()
+    setInterval(() => {
+      fetchItems()
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      {items && items.map(item => {
+        return <span style={{padding:'0px 4px'}} key={item.name} className="roll-out">
+          <img src={item.img} alt='logo' width="90" style={{padding:'0px 5px'}}></img>
+          <span>{item.name}</span>
+        </span>
+      })}
+    </>
+  )
+}
+
+export default App;
